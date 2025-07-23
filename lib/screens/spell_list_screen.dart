a8b5320f9eaa0624637f23b5fdbbb4097d0e78ad
Laura Garzon <148497204+LauraGarzon28@users.noreply.github.com>
2025-07-23 15:51:33 -0500

    classes created

---
+++ lib/screens/spell_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpellListScreen extends StatelessWidget {
+  const SpellListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
