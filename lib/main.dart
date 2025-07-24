3bf694ac6e8ba939796ac46afad03d46aa50189d
Laura Garzon <148497204+LauraGarzon28@users.noreply.github.com>
2025-07-24 15:24:10 -0500

    Spell attributes added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hedwig_hp_assistant/providers/spell_provider.dart';
 import 'package:hedwig_hp_assistant/screens/add_spell_screen.dart';
 import 'package:hedwig_hp_assistant/screens/home_screen.dart';
 import 'package:hedwig_hp_assistant/screens/settings_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:hedwig_hp_assistant/screens/spell_list_screen.dart';
 
 void main() {
@@ -20,18 +21,5 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hedwig Assistant',
-      theme: ThemeData(
-        
-      ),
-    );
-  }
-}
 
 
